60c8f28ef97a36f201a2232480812323888afcfa
Mark Miles <mark.miles@students.fhu.edu>
2024-02-02 13:45:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    txt_forward = word.replace(' ', '').upper()
+    txt_backward = word [::-1]
+    if txt_forward == txt_backward:
+        if txt_forward == '':
+            return False
+        else:
+            return True
+    else:
+        return False
 
-#YOUR CODE GOES HERE
+user_word = input()
+palindrome(user_word)
