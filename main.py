176ce1a7bf88969d74cd6e42b4b7ba20fbbadd89
Mark Miles <123099600+markmiles19@users.noreply.github.com>
2024-02-02 14:23:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
     txt_forward = word.replace(' ', '').upper()
-    txt_backward = word [::-1].upper()
+    txt_backward = word [::-1]replace(' ', '').upper()
     if txt_forward == txt_backward:
         if txt_forward == '':
             return False
